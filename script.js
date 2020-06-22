b8f1d48fbbe97fb03390b641e3a1cd858083ccbb
fakeboyas <ovoivi9@gmail.com>
2020-06-22 10:53:49 +0700

    soal 2

--- script.js
+++ script.js
@@ -29,12 +29,45 @@ class BangunDatar {
         return `Luas = ${this.luas}, Keliling = ${this.keliling}`;
     }
 
-    lingkarang() {
+    lingkaran() {
         this.luas = Math.PI * this.r * this.r;
         this.keliling = 2 * Math.PI * this.r;
         return `Luas = ${this.luas}, Keliling = ${this.keliling}`;
     }
 }
 
-const persegi1 = new BangunDatar(4);
-console.log(persegi1.persegi());
+let soal1 = new BangunDatar(5, 4, 3, 8, 2, 7);
+console.log(soal1.persegi());
+console.log(soal1.lingkaran());
+
+class BangunRuang extends BangunDatar {
+    constructor(panjang, lebar, tinggi, sisi) {
+        super(panjang, lebar, tinggi, sisi);
+        (this.panjang = panjang),
+            (this.lebar = lebar),
+            (this.tinggi = tinggi),
+            (this.sisi = sisi);
+    }
+
+    kubus() {
+        super.persegi();
+        this.volume = this.sisi * this.luas;
+        let luasPermukaan = 6 * this.luas;
+        let kelilingPermukaan = 3 * this.keliling;
+        return `Luas Permukaan = ${luasPermukaan}, Keliling = ${kelilingPermukaan}, Volume = ${this.volume}`;
+    }
+
+    balok() {
+        super.persegiPanjang();
+        this.volume = this.tinggi * this.luas;
+        let luasPermukaan =
+            2 *
+            (this.luas + this.panjang * this.tinggi + this.lebar * this.tinggi);
+        return `Luas Permukaan = ${luasPermukaan}, Volume = ${this.volume}`;
+    }
+}
+
+let soal2 = new BangunRuang(10, 6, 4, 3);
+
+console.log(soal2.balok());
+console.log(soal2.kubus());
